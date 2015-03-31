8b4dcd78ab7eef7802061b61e8085cfc94c44fae
arisimoes <arisimoes@gmail.com>
2015-03-31 09:53:09 -0300

    Comecei a fazer a classe anuciante

---
+++ talkShow/app/src/main/java/Model_Anunc/Anunciante.java
@@ -0,0 +1,26 @@
+package Model_Anunc;
+
+import java.util.Date;
+
+/**
+ * Created by root on 31/03/15.
+ */
+public class Anunciante {
+    String nome, end, cpf, login, senha;
+    char sexo;
+    Date dtNasc;
+
+}
+
+private Anunciante (String Nome, String End, char Sexo, String Cpf, Date DtNas, String Login, String Senha){
+
+    nome = Nome;
+    end = End;
+    sexo = Sexo;
+    cpf = Cpf;
+    dtNasc = DtNas;
+    login = Login;
+    senha = Senha;
+
+
+        }
