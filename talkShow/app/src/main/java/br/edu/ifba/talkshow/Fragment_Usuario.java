33601ec7e5c58de7485e29f413c0032d72908141
AlexsandroSantos <alexsandro.santos@camacari.ba.gov.br>
2015-04-08 13:04:59 -0300

    Atualizando o layout usuario

--- talkShow/app/src/main/java/br/edu/ifba/talkshow/Fragment_Usuario.java
+++ talkShow/app/src/main/java/br/edu/ifba/talkshow/Fragment_Usuario.java
@@ -14,38 +14,18 @@ import android.view.ViewGroup;
  * Activities that contain this fragment must implement the
  * {@link Fragment_Usuario.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Fragment_Usuario#newInstance} factory method to
+ * Use the {@link Fragment_Usuario#//newInstance} factory method to
  * create an instance of this fragment.
  */
 public class Fragment_Usuario extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_Usuario.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment_Usuario newInstance(String param1, String param2) {
-        Fragment_Usuario fragment = new Fragment_Usuario();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    /*// TODO: Rename and change types and number of parameters
+    public static Fragment_Usuario newInstance(String param1, String param2)
+    {
+
+    }*/
 
     public Fragment_Usuario() {
         // Required empty public constructor
@@ -55,8 +35,8 @@ public class Fragment_Usuario extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+           // mParam1 = getArguments().getString(ARG_PARAM1);
+            //mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
