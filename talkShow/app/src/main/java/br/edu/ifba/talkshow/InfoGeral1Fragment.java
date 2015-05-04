75237effc830eb7658af1b730566fee19a166124
arisimoes <arisimoes@gmail.com>
2015-05-04 16:04:26 -0300

    layout novo

---
+++ talkShow/app/src/main/java/br/edu/ifba/talkshow/InfoGeral1Fragment.java
@@ -0,0 +1,37 @@
+package br.edu.ifba.talkshow;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InfoGeral1Fragment extends Fragment {
+
+    public static InfoGeral1Fragment newInstance(String param1, String param2) {
+        InfoGeral1Fragment fragment = new InfoGeral1Fragment();
+        return fragment;
+    }
+
+    public InfoGeral1Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info_geral1, container, false);
+    }
+
+}
